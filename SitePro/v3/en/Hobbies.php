778fa0d99e40a1fb206c333b013aaf55e2a0ec8c
yahyanassif <yahyanassif29@gmail.com>
2023-10-10 15:32:13 +0000

    Sitepro/v3 en

--- SitePro/v3/en/Hobbies.php
+++ SitePro/v3/en/Hobbies.php
@@ -8,17 +8,15 @@
     <h1>
         <img src="foot.jpg" alt="Football">
         <br>
-        Le football est le premier sport que j'ai fait durant ma vie,Je jouais beaucoup avec les amis mais je l'ai jamais pratiqué d'une facon professionnel. 
+        Football was the first sport I played in my life. I used to play a lot with friends but I never played it professionally. 
         <br>
         <img src="boxe.jpg" alt="Image de deux boxeurs en Duel">
         <br>
-        Les arts martiaux en générale m'interessent depuis les dernieres années. J'ai fait la boxe pendant 7 mois 
-        et cela m'a interessé beaucoup puisque tu apprends des techniques qui te servent 
-        pour la self-defense ainsi qu'elle permet d'etre de plus en plus discipliné
+        I've been interested in martial arts in general for the last few years. I did boxing for 7 months and it really interested me because you learn techniques that you can use for self-defence for self-defence as well as allowing you to be more and more disciplined
         <br>
         <img src="tennis.jpg" alt="raquette de tennis">
         <br> 
-        J'ai pratiqué le tennis en niveau amateur pendant 3 ans, et je le vois comme étant un sport competitif et qui necessite un souffle important pour survivre tout le match. 
+        I played tennis as an amateur for 3 years, and I see it as a competitive sport that requires a lot of breath to survive the whole match.
         
     
     </h1>
--- SitePro/v3/en/contact.php
+++ SitePro/v3/en/contact.php
@@ -1,7 +1,7 @@
 <section class="contact-section">
-    <h2>Contactez-Moi</h2>
-    <p><strong>Adresse:</strong> 243 Rue du kiosque 59500, Douai</p>
+    <h2>Contact me</h2>
+    <p><strong>Adress:</strong> 243 Rue du kiosque 59500, Douai</p>
     <p><strong>Email:</strong> <a href="mailto:yahyanassif29@gmail.com">yahyanassif29@gmail.com</a></p>
-    <p><strong>Téléphone:</strong> <a href="tel:+33611890744">+33 6 11 89 07 44</a></p>
+    <p><strong>Phone Number:</strong> <a href="tel:+33611890744">+33 6 11 89 07 44</a></p>
 </section>
 <a href="index.php?page=contact&lang=fr">Français</a>
