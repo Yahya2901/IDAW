e9ef1c7928ab5de5e201f7fa1f816560a5ff023c
yahyanassif <yahyanassif29@gmail.com>
2023-10-15 18:30:42 +0000

    Sitepro/v3

--- SitePro/v3/en/infos-techniques.php
+++ SitePro/v3/en/infos-techniques.php
@@ -4,5 +4,15 @@
 </head>
 
 <a href="index.php?page=infos-techniques&lang=fr">Français</a>
+<p>
+<img src="html.png" alt="Football">
+HTML is the standard mark-up language used to create and structure the content of web pages. It is made up of elements and tags that define the structure and content of a web page, such as headings, paragraphs, links, images and more. HTML is a static language, which means that web pages created with HTML are generally fixed documents that do not change according to the user's actions.
+<br>
+<img src="php-logo.png" alt="Football">
+PHP, on the other hand, is a server programming language widely used for dynamic web development. Unlike HTML, PHP can be used to create interactive, personalised web pages. It can be integrated directly into HTML code to generate dynamic content, manage forms, access databases and much more. PHP runs on the server side, which means that the PHP code is processed on the server before the result is sent to the user's browser, enabling interactive and personalised websites to be created. Modern websites often use a combination of HTML, PHP, and other technologies to deliver a rich and dynamic user experience.
+<br>
+<img src="css.png" alt="Football">
+CSS is a style sheet language used to define the presentation and layout of web pages created in HTML. Whereas HTML is used to structure the content of a web page, CSS is used to control its appearance, including colours, fonts, margins, spacing, layout and other visual aspects.
 
+CSS rules are applied to HTML elements via selectors, which allow specific elements to be targeted and styles to be applied to them. CSS offers great flexibility, allowing you to customise the design of a web page extensively, while maintaining a clear separation between content (HTML) and presentation (CSS). There are also advanced techniques in CSS, such as media queries for responsive design, animations, transitions, and many others, which can be used to create modern, attractive websites.
 <hr>
--- SitePro/v3/fr/infos-techniques.php
+++ SitePro/v3/fr/infos-techniques.php
@@ -4,5 +4,15 @@
 <meta charset="utf-8">
 </head>
 <a href="index.php?page=infos-techniques&lang=en">Anglais</a>
+<p>
+<img src="html.png" alt="Football">
+HTML est le langage de balisage standard utilisé pour créer et structurer le contenu des pages web. Il est composé d'éléments et de balises qui définissent la structure et le contenu d'une page web, tels que les titres, les paragraphes, les liens, les images, et plus encore. HTML est un langage statique, ce qui signifie que les pages web créées avec HTML sont généralement des documents fixes qui ne changent pas en fonction des actions de l'utilisateur.
+<br>
+<img src="php-logo.png" alt="Football">
+D'un autre côté, PHP est un langage de programmation serveur largement utilisé pour le développement web dynamique. Contrairement à HTML, PHP permet de créer des pages web interactives et personnalisées. Il peut être intégré directement dans le code HTML pour générer du contenu dynamique, gérer des formulaires, accéder à des bases de données, et bien plus encore. PHP s'exécute côté serveur, ce qui signifie que le code PHP est traité sur le serveur avant que le résultat soit envoyé au navigateur de l'utilisateur, permettant ainsi de créer des sites web interactifs et personnalisés. Les sites web modernes utilisent souvent une combinaison d'HTML, PHP, et d'autres technologies pour offrir une expérience utilisateur riche et dynamique.
+<br>
+<img src="css.png" alt="Football">
+CSS est un langage de feuilles de style utilisé pour définir la présentation et la mise en forme des pages web créées en HTML. Alors qu'HTML sert à structurer le contenu d'une page web, CSS permet de contrôler son apparence, notamment les couleurs, les polices de caractères, les marges, les espacements, les mises en page, et d'autres aspects visuels.
 
+Les règles CSS sont appliquées aux éléments HTML via des sélecteurs, qui permettent de cibler des éléments spécifiques et d'appliquer des styles à ceux-ci. CSS offre une grande flexibilité, ce qui permet de personnaliser le design d'une page web de manière approfondie, tout en maintenant une séparation claire entre le contenu (HTML) et la présentation (CSS). Il existe également des techniques avancées en CSS, telles que les médias queries pour la conception réactive, les animations, les transitions, et bien d'autres, qui permettent de créer des sites web modernes et attrayants.
 <hr>
