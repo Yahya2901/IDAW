8816f47cbf7c78e5d8c1f8793236275318bb361e
yahyanassif <yahyanassif29@gmail.com>
2023-11-03 22:28:22 +0000

    .

--- Projet/Backend/Accueil.php
+++ Projet/Backend/Accueil.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" />
     <title>User Management</title>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -14,62 +15,19 @@
 <body>
     <header>
         <h1>Bienvenue sur iMangerMieux</h1>
-        <p>Un Outil Simple pour suivre vos aliments et calories, pour une vie saine et energetique.</p>
+        
        
     </header>
 
     <nav>
         <ul>
             <li><a href="accueil.php">Accueil</a></li>
-            <li><a href="suivi.php">Historique des aliments</a></li>
+            <li><a href="Historique des aliments.php">Historique des aliments</a></li>
             <li><a href="profil.php">Voir mon Profil</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
         </ul>
     </nav>
     <body>
-    <div class="circle">
-        <div class="calories">0</div>
-        <div class="label">Calories</div>
-    </div>
-
-    <script src="script.js"></script>
-
-    <footer>
-        <p>&copy; 2023 Tableau de Bord de Suivi de Nutrition.</p>
-    </footer>
-
-    <form action="logout.php" method="POST">
-        <button type="submit">Se déconnecter</button>
-    </form>
-</body>
-</html>
-    <footer>
-        <p>&copy; 2023 iMangerMieux.</p>
-    </footer>
-    
-</head>
-<body>
-
-<!-- Table HTML -->
-<table id="usersTable" class="display">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Calories</th>
-        </tr>
-    </thead>
-    <tbody></tbody>
-</table>
-
-<!-- Modal for editing users -->
-<div id="editModal" style="display:none; position:fixed; top:50%; left:50%; transform: translate(-50%, -50%); background:white; padding:20px;">
-    <h2>Edit User</h2>
-    <form id="editForm">
-        <input type="hidden" id="userId" name="userId">
-        Name: <input type="text" id="userName" name="userName"><br><br>
-        Email: <input type="text" id="userEmail" name="userEmail"><br><br>
-        <input type="button" value="Save" onclick="saveEdit()">
-        <input type="button" value="Cancel" onclick="closeEdit()">
-    </form>
-</div>
+        <p>Bienvenue sur iMANGERMIEUX, votre compagnon intelligent pour une alimentation saine et équilibrée. Notre application de tracking de calories est conçue pour vous aider à atteindre vos objectifs de santé, que vous cherchiez à perdre du poids, à gagner en masse musculaire ou simplement à adopter de meilleures habitudes alimentaires. Avec iMANGERMIEUX, vous pouvez facilement suivre votre apport calorique quotidien, enregistrer vos repas et collations, explorer une vaste base de données d'aliments, et obtenir des informations précieuses sur votre alimentation. Notre objectif est de vous donner le contrôle sur votre nutrition, de manière simple et efficace. Rejoignez-nous dès aujourd'hui et commencez à manger mieux avec iMANGERMIEUX !</p>
+        
+    <p><a href="logout.php">Se déconnecter</a></p>
--- Projet/Backend/register.php
+++ Projet/Backend/register.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> iMangerMieux - Tracker d'Aliments et de Calories </title>
+    <meta charset="utf-8" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" />
+    <title>User Management</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+</head>
 <?php
 session_start();
 
