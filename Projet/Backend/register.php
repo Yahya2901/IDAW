0c56da49e03a107478074d74f150da5844e81409
yahyanassif <yahyanassif29@gmail.com>
2023-11-03 17:29:48 +0000

    .

--- Projet/Backend/register.php
+++ Projet/Backend/register.php
@@ -23,25 +23,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Après l'inscription, stockez les informations de l'utilisateur dans la session
     $_SESSION['user_id'] = $user_id; // Assurez-vous d'obtenir l'ID de l'utilisateur après l'insertion
 
-    
     header('Location: Accueil.php');
     exit();
 }
 ?>
 
-<!-- Formulaire d'inscription -->
-<form method="POST" action="register.php">
-    <input type="text" name="login" placeholder="Login" required>
-    <input type="password" name="password" placeholder="Mot de passe" required> <!-- Champ de mot de passe -->
-    <input type="number" name="age" placeholder="Age" required>
+<!-- Ajout du paragraphe "Inscrivez-vous" -->
+<h2 style="text-align: center;">Inscrivez-vous</h2>
+
+<!-- Formulaire d'inscription avec les cases centrées -->
+<form method="POST" action="register.php" style="text-align: center;">
+    <input type="text" name="login" placeholder="Login" required><br>
+    <input type="password" name="password" placeholder="Mot de passe" required><br> <!-- Champ de mot de passe -->
+    <input type="number" name="age" placeholder="Age" required><br>
     <select name="gender">
         <option value="homme">Homme</option>
         <option value="femme">Femme</option>
-    </select>
+    </select><br>
     <select name="activity_level">
         <option value="bas">Bas</option>
         <option value="moyen">Moyen</option>
         <option value="élevé">Élevé</option>
-    </select>
+    </select><br>
     <input type="submit" value="S'inscrire">
 </form>
