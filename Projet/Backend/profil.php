f3d677d13d1e1e0928180845ffc410c380112374
yahyanassif <yahyanassif29@gmail.com>
2023-11-06 18:42:31 +0000

    .

--- Projet/Backend/profil.php
+++ Projet/Backend/profil.php
@@ -4,6 +4,7 @@
     <title> iMangerMieux - Tracker d'Aliments et de Calories </title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="styles.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
