1169fe7214ed372e698ebf6659a6c6b32a68e8c6
yahyanassif <yahyanassif29@gmail.com>
2023-11-07 16:14:56 +0000

    .

--- Projet/Backend/Accueil.php
+++ Projet/Backend/Accueil.php
@@ -4,6 +4,7 @@
     <title> iMangerMieux - Tracker d'Aliments et de Calories </title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"  href="http://localhost/IDAW/Projet/Frontend/css/styles.css">
     <title>Accueil</title>
 </head>
 
@@ -24,5 +25,7 @@
     </nav>
     <body>
         <p>Bienvenue sur iMANGERMIEUX, votre compagnon intelligent pour une alimentation saine et équilibrée. Notre application de tracking de calories est conçue pour vous aider à atteindre vos objectifs de santé, que vous cherchiez à perdre du poids, à gagner en masse musculaire ou simplement à adopter de meilleures habitudes alimentaires. Avec iMANGERMIEUX, vous pouvez facilement suivre votre apport calorique quotidien, enregistrer vos repas et collations, explorer une vaste base de données d'aliments, et obtenir des informations précieuses sur votre alimentation. Notre objectif est de vous donner le contrôle sur votre nutrition, de manière simple et efficace. Rejoignez-nous dès aujourd'hui et commencez à manger mieux avec iMANGERMIEUX !</p>
-
+        <div style="text-align: center;">
+    <img src="iMangerMieux.png" alt="">
+        </div>
     <p><a href="logout.php">Se déconnecter</a></p>
