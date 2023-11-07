86ae1c1ba68d18e10bf886e666ad205c2159f776
yahyanassif <yahyanassif29@gmail.com>
2023-11-07 16:36:59 +0000

    .

--- Projet/Backend/Accueil.php
+++ Projet/Backend/Accueil.php
@@ -4,7 +4,6 @@
     <title> iMangerMieux - Tracker d'Aliments et de Calories </title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet"  href="http://localhost/IDAW/Projet/Frontend/css/styles.css">
     <title>Accueil</title>
 </head>
 
@@ -12,7 +11,6 @@
     <header>
         <h1>Bienvenue sur iMangerMieux</h1>
     </header>
-
     <nav>
         <ul>
             <li><a href="accueil.php">Accueil</a></li>
@@ -20,7 +18,7 @@
             <li><a href="Historique des aliments.php">Historique des aliments</a></li>
             <li><a href="profil.php">Voir mon Profil</a></li>
             <li><a href="sport.php">Sports</a></li>
-            
+            <li><a href="logout.php">Deconnexion</a></li>
         </ul>
     </nav>
     <body>
@@ -28,4 +26,169 @@
         <div style="text-align: center;">
     <img src="iMangerMieux.png" alt="">
         </div>
-    <p><a href="logout.php">Se déconnecter</a></p>
+    
+<style>
+    body {
+    font-family: 'Arial', sans-serif;
+    line-height: 1.6;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+}
+
+header {
+    background: #50b3a2;
+    color: #fff;
+    padding-top: 30px;
+    min-height: 70px;
+    border-bottom: #bbb 1px solid;
+}
+
+header a {
+    color: #ffffff;
+    margin-top: 20px;
+    padding-right: 15px;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 16px;
+}
+
+nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #333;
+    padding: 10px 0;
+}
+
+nav a {
+    color: white;
+    padding: 0 15px;
+    text-decoration: none;
+}
+
+nav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+nav ul {
+    padding: 0;
+    list-style: none;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+section {
+    margin: 15px;
+    padding: 15px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    text-align: center;
+    padding: 40px;
+}
+
+section h2, section h3 {
+    color: #333;
+}
+
+section h1{
+    font-size: 2.5rem;
+  margin-bottom: 20px;
+  color: #333;
+}
+section p {
+    text-align: justify;
+    font-size: 1.2rem;
+  margin: 20px 0;
+  color: #666;
+}
+
+section ul {
+    list-style: square;
+    padding-left: 20px;
+}
+
+footer {
+    text-align: center;
+    padding: 1px 1px;
+    background: #50b3a2;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    color: white;
+    width: 100%;
+}
+
+.footer p {
+    margin: 0;
+    padding: 0;
+}
+.image-container {
+    text-align: center;
+  }
+  
+  .image-container img {
+    width: 200px;
+    border-radius: 50%;
+  }
+  pre {
+    font-size: 2em;
+    line-height: 0.6;
+  }
+#currentpage {
+    background-color: #50b3a2;
+    font-weight: bold;
+    color: #fafafa;
+}
+.center-vertical {
+    display: flex;
+    align-items: center;
+    height: 100vh;
+  }
+  .bandeau_haut {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 20px;
+    background-color: #50b3a2;
+}
+
+.titre {
+    margin: 0;
+    font-size: 2em;
+    color: #333;
+}
+.contact-section {
+    padding: 20px;
+    background-color: #f9f9f9;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    margin: 20px 0;
+}
+
+.contact-section h2 {
+    font-size: 24px;
+    margin-bottom: 20px;
+    color: #333;
+}
+
+.contact-section p {
+    font-size: 18px;
+    line-height: 1.6;
+    color: #000000;
+}
+
+.contact-section a {
+    color: #0066cc;
+    text-decoration: none;
+}
+
+.contact-section a:hover {
+    text-decoration: underline;
+}
+</style>
