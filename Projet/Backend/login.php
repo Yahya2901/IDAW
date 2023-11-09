75866c9f17542b26d6e191dab50e5bb4c27512e7
yahyanassif <yahyanassif29@gmail.com>
2023-11-09 22:23:38 +0000

    .

--- Projet/Backend/login.php
+++ Projet/Backend/login.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
-        <link rel="stylesheet"  href="http://localhost/IDAW/Projet/Frontend/css/style.css">
-        <?php
+    <link rel="stylesheet"  href="http://localhost/IDAW/Projet/Frontend/css/style.css">
+        
+              
+
+    
+
+<?php
 // Connexion à la base de données
 require_once 'config.php';
 
@@ -81,6 +86,7 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
