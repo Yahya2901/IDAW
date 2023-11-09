6b00a435a4a20e26e0047a3f7adc25821800e8fa
yahyanassif <yahyanassif29@gmail.com>
2023-11-09 21:24:29 +0000

    .

--- Projet/Backend/login.php
+++ Projet/Backend/login.php
@@ -1,6 +1,10 @@
 <html>
     <head>
-        <meta charset="utf-8" />
+    <title> iMangerMieux - Tracker d'Aliments et de Calories </title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
         <link rel="stylesheet"  href="http://localhost/IDAW/Projet/Frontend/css/style.css">
 <?php
 
